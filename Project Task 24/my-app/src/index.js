5db93f7dea1ec1d919748841b8a180244c7e14eb
ashishbarik40 <87799586+ashishbarik40@users.noreply.github.com>
2023-06-07 10:25:43 +0530

    Add files via upload

---
+++ Project Task 24/my-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Random from './Random';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Random/>
+  </React.StrictMode>
+);
